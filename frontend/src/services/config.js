0cc0d5331303bb4344746ff3e67fed1f95c92020
Sampo Rapeli <35733458+samporapeli@users.noreply.github.com>
2022-05-28 14:30:33 +0300

    Add dockerized production setup

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,5 +1,6 @@
-// const api = 'http://localhost:8080/api/v1/restaurants'
-const api = 'http://localhost:3000/api/v1/'
+const api = process.env.DEVELOPMENT
+  ? 'http://localhost:3000/api/v1/'
+  : '/api/v1/'
 const config = {
   api
 }
